cf1e1534dab912e597eb145cb3c270ea92f4dddd
o-thorburn <123601696+o-thorburn@users.noreply.github.com>
2023-06-02 11:23:53 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 # When you edit this document, it will update the web app at 'https://o-thorburn-streamlit-app-jabojx.streamlit.app' 
 
+import streamlit as st
+
 st.title('My CE App') # Sets the the title of your page
 st.header('Data Visualization Section')  # Sets a header for a section
 st.subheader('Subsection: Pie Chart Analysis')  # Sets a subheader for a subsection
