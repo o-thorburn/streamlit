a76c00562059226214282c2944069e354fe63624
o-thorburn <123601696+o-thorburn@users.noreply.github.com>
2023-06-02 11:11:47 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
 # When you edit this document, it will update the web app at 'https://o-thorburn-streamlit-app-jabojx.streamlit.app' 
 
+st.title('My CE App') # Sets the the title of your page
+st.header('Data Visualization Section')  # Sets a header for a section
+st.subheader('Subsection: Pie Chart Analysis')  # Sets a subheader for a subsection
+st.text('This section focuses on data preprocessing.')
+st.markdown('**This is some bold text**')
+import pandas as pd
